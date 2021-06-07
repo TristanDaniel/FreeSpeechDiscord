e16988806cbe847081d68f00dab564b1de5cfb60
Tristan <tristandaniel@notrealemail.com>
2021-06-07 11:04:07 -0500

    added distinction for different users talking in a conversation

---
+++ development/freespeechbot.py
@@ -0,0 +1,31 @@
+import discord
+
+token = 'ODUwMzc4Njk3MTYzNjY5NTA0.YLo24A.28qCdAJBhXEIaIxtlFGJBjjavww'
+targetchid = 850381224131821599
+client = discord.Client()
+lastuser = client
+
+@client.event
+async def on_ready():
+	print('logged in')
+
+@client.event
+async def on_message(message):
+	global lastuser
+	msgcontent = message.content
+	msgauthor = message.author
+	if not(message.author == client.user) and isinstance(message.channel, discord.DMChannel):
+		targetch = client.get_channel(targetchid)
+		if len(msgcontent) > 0:
+			if not (lastuser == msgauthor):
+				msgcontent = "--\n" + msgcontent
+				lastuser = msgauthor
+			await targetch.send(msgcontent)
+		if message.attachments:
+			if not (lastuser == msgauthor):
+				msgcontent = "--\n" + msgcontent
+				lastuser = msgauthor
+			for i in range(len(message.attachments)):
+				await targetch.send(message.attachments[i].url)
+
+client.run(token)
