0862956f6290f06d94fded2e1fdfd8f399faf831
Tristan <tristandaniel@notrealemail.com>
2021-06-07 14:59:14 -0500

    text and attachments are all sent at once now. method of using bot has fully been migrated to use individual servers, and then send messages to all other servers into the specified channel

--- working/freespeechbot.py
+++ working/freespeechbot.py
@@ -1,7 +1,6 @@
 import discord
 
 token = 'bot token'
-targetchid = [channel id]
 client = discord.Client()
 lastuser = client
 
@@ -11,29 +10,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	c = []
-	for g in client.guilds:
-		c.append(discord.utils.get(g.channels, name="free-speech"))
 	global lastuser
 	msgcontent = message.content
 	msgauthor = message.author
-	if not(message.author == client.user) and isinstance(message.channel, discord.DMChannel):
-		targetch = client.get_channel(targetchid)
-		if len(msgcontent) > 0:
-			if not (lastuser == msgauthor):
-				msgcontent = "--\n" + msgcontent
-				lastuser = msgauthor
-			for i in range(len(c)):
-				if not(c[i] == None):
-					await c[i].send(msgcontent)
+	if not(message.author == client.user) and not (isinstance(message.channel, discord.DMChannel)):
+		if (message.channel.name != "free-speech"):
+			return
+		c = []
+		for g in client.guilds:
+			c.append(discord.utils.get(g.channels, name="free-speech"))
+		msgchannel = message.channel.id
+		if not (lastuser == msgauthor):
+			msgcontent = "--\n" + msgcontent
+			lastuser = msgauthor
 		if message.attachments:
 			attachments = message.attachments
-			if not (lastuser == msgauthor):
-				msgcontent = "--\n" + msgcontent
-				lastuser = msgauthor
-			for j in range(len(c)):
-				if not(c[j] == None):
-					for i in range(len(attachments)):
-						await c[j].send(message.attachments[i].url)
+			for i in range(len(attachments)):
+				msgcontent = msgcontent + "\n" + attachments[i].url
+		for i in c:
+			if not(i == None or i.id == msgchannel):
+				await i.send(msgcontent)
 
 client.run(token)
